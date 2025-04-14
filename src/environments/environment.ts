a06cb4b7109826c46a4cceecec84793c062c70dc
Uver Diaz <front.desarrollo@ideascontrol.com>
2025-04-14 12:12:59 -0500

    fix: update API URLs in environment files to use HTTPS

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: false,
-  apiUrl: 'vm-api-backend-appservice-cvgmb8evdwh3cha5.canadacentral-01.azurewebsites.net'
+  apiUrl: 'https://vm-api-backend-appservice-cvgmb8evdwh3cha5.canadacentral-01.azurewebsites.net'
 }; 
